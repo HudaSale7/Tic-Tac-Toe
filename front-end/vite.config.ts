c6a9e0f9125b3f9baa16c99583f2e8de7e00bce0
HudaSale7 <hudasaleh640@gmail.com>
2023-08-17 02:32:30 +0300

    deploy the game

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https:///HudaSale7.github.io/Tic-Tac-Toe/",
 });
