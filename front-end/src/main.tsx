a14de0373a51a8435bd605c5cee95b3b958acaa7
HudaSale7 <hudasaleh640@gmail.com>
2023-08-17 02:46:01 +0300

    deploy the game

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,7 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './styles/index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./styles/index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/Tic-Tac-Toe/">
     <App />
-)
+  </BrowserRouter>
+);
